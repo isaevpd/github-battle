3387fb07f821c87fcc05806c65ea5e222332b25b
Pavel Isaev <pavel.isaev@wolt.com>
2019-07-17 00:24:51 +0300

    Add Nav and Popular components

---
+++ app/components/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Popular from './Popular';
+import * as ReactRouter from 'react-router-dom';
+import Nav from './Nav';
+import Home from './Home';
+import Battle from './Battle';
+import Switch from 'react-router-dom/es/Switch';
+
+const Router = ReactRouter.BrowserRouter;
+const Route = ReactRouter.Route;
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div className='container'>
+          <Nav/>
+          <Switch>
+            <Route exact path='/' component={Home}/>
+            <Route exact path='/battle' component={Battle}/>
+            <Route path='/popular' component={Popular}/>
+            <Route render={() => <p>Not found!</p>}/>
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
+
+export default App
---
+++ app/components/Popular.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import * as api from '../utils/api'
+import PropTypes from 'prop-types';
+
+
+const SelectLanguage = ({ selectedLanguage, onSelect }) => {
+  const languages = ['All', 'Javascript', 'Ruby', 'Java', 'CSS', 'Python'];
+  return (
+    <ul className="languages">
+      {languages.map(lang => (
+        <li
+          style={lang === selectedLanguage ? { color: '#d0021b' } : null}
+          onClick={() => onSelect(lang)}
+          key={lang}
+        >
+          {lang}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+
+const RepoGrid = ({repos}) => {
+  return (
+    <ul className='popular-list'>
+      {repos.map(function(repo, index) {
+        return (
+          <li key={repo.name} className='popular-item'>
+            <div className='popular-rank'>#{index + 1}</div>
+            <ul className='space-list-items'>
+              <li>
+                <img
+                  className='avatar'
+                  src={repo.owner.avatar_url}
+                  alt={`Avatar for ${repo.owner.login}`}
+                />
+              </li>
+              <li><a href={repo.html_url}>{repo.name}</a></li>
+              <li>@{repo.owner.login}</li>
+              <li>{repo.stargazers_count}</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+RepoGrid.propTypes = {
+  repos: PropTypes.array.isRequired,
+}
+
+
+class Popular extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedLanguage: 'All',
+      repos: null,
+    };
+    this.updateLanguage = this.updateLanguage.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateLanguage(this.state.selectedLanguage);
+  }
+
+  updateLanguage(lang) {
+    this.setState({
+      selectedLanguage: lang,
+    });
+    api.fetchPopularRepos(lang).then(repos =>
+      this.setState({
+        repos: repos,
+      }),
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        <SelectLanguage
+          selectedLanguage={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
+        {!this.state.repos ? <p>LOADING!</p> : <RepoGrid repos={this.state.repos}/>}
+      </div>
+    );
+  }
+}
+
+export default Popular;
